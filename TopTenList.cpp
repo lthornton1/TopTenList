7b989ce99bbcd2c9f8b87903ec7d70f3c88ddbe5
Logen_Thornton <lthornton1@mail.csuchico.edu>
2020-04-03 22:19:18 -0700

    implement TopTenList.cpp and .h

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -7,15 +7,31 @@ using std::string;
 
 TopTenList::TopTenList()
 {
+  _list.resize(10);
+}
 
+void TopTenList::display_forward(){
+  cout<<"Printing Top 10 in Forward order:"<<endl;
+  for(int i = 0; i < (int)_list.size() ; i++){
+    Hyperlink temp = get(i);
+        cout<<"Number "<<i+1<<": "<<temp.text<<", url: "<<temp.url<<endl;
+  }
+}
+
+void TopTenList::display_backward(){
+  cout<<"Printing Top 10 in Backward order:"<<endl;
+  for(int i = (int)_list.size()-1; i >= 0 ; i--){
+    Hyperlink temp = get(i);
+    cout<<"Number "<<i+1<<": "<<temp.text<<", url: "<<temp.url<<endl;
+  }
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-
+  _list[index-1] = link;
 }
 
 Hyperlink TopTenList::get(int index)
 {
-
+  return _list[index];
 }
--- TopTenList.h
+++ TopTenList.h
@@ -1,18 +1,26 @@
 //Manages a list of top ten hyperlinks
 #ifndef TOPTENLIST_H
 #define TOPTENLIST_H
+#include <iostream>
 #include <string>
 #include <vector>
+#include "IDirectionable.h"
 #include "Hyperlink.h"
 
 using std::string;
 using std::vector;
+using std::cout;
+using std::endl;
 
-class TopTenList
+class TopTenList : public IDirectionable
 {
   private:
+    vector<Hyperlink> _list;
 
   public:
+    TopTenList();
+    void display_forward();
+    void display_backward();
     void set_at(int index, Hyperlink link);
     Hyperlink get(int index);
 };
